3733ba51e1c3b38134a070d3ac3021dfcce84901
1054009064 <44148148+1054009064@users.noreply.github.com>
2023-03-24 09:17:00 +0800

    Add feeds 'vssr'

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,15 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwallp https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
-echo 'src-git passwalll https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwallp https://github.com/xiaorouji/openwrt-passwall;packages' >> feeds.conf.default
+echo 'src-git passwalll https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+
 
 git clone -b "18.06" https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-theme-argon-config
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean//luci-app-vssr
 
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/lean
